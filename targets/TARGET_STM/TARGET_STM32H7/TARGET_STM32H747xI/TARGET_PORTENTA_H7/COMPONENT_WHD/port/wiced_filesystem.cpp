3655756dee36ccaa2a0ec4ee91d6871f143fc686
pennam <m.pennasilico@arduino.cc>
2021-07-22 12:04:53 +0200

    implement and use get_default_instance for QSPIBD

--- targets/TARGET_STM/TARGET_STM32H7/TARGET_STM32H747xI/TARGET_PORTENTA_H7/COMPONENT_WHD/port/wiced_filesystem.cpp
+++ targets/TARGET_STM/TARGET_STM32H7/TARGET_STM32H747xI/TARGET_PORTENTA_H7/COMPONENT_WHD/port/wiced_filesystem.cpp
@@ -36,11 +36,18 @@
 #define WIFI_DEFAULT_PARTITION 1
 #define WIFI_DEFAULT_FS 0
 
-QSPIFBlockDevice *qspi_bd = NULL;
+BlockDevice      *qspi_bd = NULL;
 MBRBlockDevice   *mbr_bd = NULL;
 FATFileSystem    *wifi_fs = NULL;
 
 wiced_filesystem_t resource_fs_handle;
+
+
+MBED_WEAK BlockDevice *BlockDevice::get_default_instance()
+{
+    static QSPIFBlockDevice default_bd(PD_11, PD_12, PF_7, PD_13,  PF_10, PG_6, QSPIF_POLARITY_MODE_1, 40000000);
+    return &default_bd;
+}
 
 MBED_WEAK void wiced_filesystem_mount_error(void)
 {
@@ -134,7 +141,7 @@ wiced_result_t wiced_filesystem_init(void)
 {
     if (mbr_bd == NULL && wifi_fs == NULL) {
         WPRINT_WHD_DEBUG(("Initialize FileSystem with Mbed default settings\n\r"));
-        qspi_bd = new QSPIFBlockDevice(PD_11, PD_12, PF_7, PD_13,  PF_10, PG_6, QSPIF_POLARITY_MODE_1, 40000000);
+        qspi_bd = mbed::BlockDevice::get_default_instance();
 
         if (qspi_bd->init() == BD_ERROR_OK) {
             mbr_bd = new MBRBlockDevice(qspi_bd, WIFI_DEFAULT_PARTITION);
@@ -154,7 +161,7 @@ wiced_result_t wiced_filesystem_init(void)
 wiced_result_t wiced_filesystem_mount(BlockDevice *device, wiced_filesystem_handle_type_t fs_type, wiced_filesystem_t *fs_handle_out, const char *mounted_name)
 {
     wifi_fs = new FATFileSystem(mounted_name);
-
+    WPRINT_WHD_DEBUG(("wiced_filesystem_mount\n\r")); 
     int err = wifi_fs->mount(device);
     whd_firmware_check_hook(mounted_name, err);
     if (!err) {
@@ -178,7 +185,7 @@ wiced_result_t wiced_filesystem_file_open(wiced_filesystem_t *fs_handle, wiced_f
     if (wifi_fs == NULL) {
         return WICED_ERROR;
     }
-
+    //WPRINT_WHD_DEBUG(("wiced_filesystem_file_open\n\r")); 
     *file_handle_out = open(filename, mode);
     if (*file_handle_out == -1) {
         return WICED_ERROR;
